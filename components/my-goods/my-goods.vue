775a6c1f66f3c92d70e52c06bfbd7a0993dd1e81
Tejimeng <1104732951@qq.com>
2023-07-18 23:39:40 +0800

    完成了商品列表的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,73 @@
+<template>
+	<view class="goods-item">
+		<!-- 商品左侧图片区域 -->
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+		</view>
+		<!-- 商品右侧信息区域 -->
+		<view class="goods-item-right">
+			<!-- 商品标题 -->
+			<view class="goods-name">{{goods.goods_name}}</view>
+			<view class="goods-info-box">
+				<!-- 商品价格 -->
+				<view class="goods-price">￥{{tofixed}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			goods:{
+				type:Object,
+				default:{}
+			}
+		},
+		data() {
+			return {
+				// 默认的图片
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		},
+		computed:{
+			tofixed() {
+			    return Number(this.goods.goods_price).toFixed(2)
+			  }
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+
+		.goods-item-left {
+			margin-right: 5px;
+
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-price {
+				font-size: 16px;
+				color: #c00000;
+			}
+		}
+	}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		sdsad
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +13,78 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求体参数
+				queryObj: {
+					// 查询的关键字
+					query: '',
+					// 商品分类id
+					cid: '',
+					// 页码值
+					pagenum: 1,
+					// 每页显示的数据
+					pagesize: 10
+				},
+				// 商品列表的数据
+				goodsList: [],
+				// 总的数量，用来实现分页
+				total: 0,
+				// 是否正在请求数据
+				isloading: false
 			};
+		},
+		onLoad(options) {
+			// 将页面参数转存到 this.queryObj 对象中
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			// 调用获取商品列表数据的方法
+			this.getGoodsList()
+		},
+		methods: {
+			// 获取商品列表数据的方法
+			async getGoodsList(callBack) {
+				this.isloading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isloading = false
+				// 如果正在下拉刷新，则立即停止
+				callBack&&callBack()
+				if (res.meta.status !== 200) return
+				// 为数据合并
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			// 跳转到商品的详情页
+			gotoDetail(goods){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id='+goods.goods_id
+				})
+			}
+		},
+		// 触底的事件
+		onReachBottom() {
+			// 判断是否所有的数据已经加载完毕
+			if (this.queryObj.pagenum * this.queryObj.pagesize > this.total) return uni.showToast({
+				title:"已经到底啦！",
+				duration:100,
+				icon: 'none',
+			})
+			// 判断是否正在请求其它数据，如果是，则不发起额外的请求
+			if (this.isloading) return
+
+			this.queryObj.pagenum += 1
+			this.getGoodsList()
+		},
+		// 下拉刷新的事件
+		onPullDownRefresh() {
+			// 重置一些页面的数据
+			this.queryObj.pagenum=1
+			this.total=0
+			this.isloading=false
+			this.goodsList=[]
+			// 重新发起请求，同时传入停止下拉刷新的回调函数
+			this.getGoodsList(()=>{uni.stopPullDownRefresh()})
+			
 		}
 	}
 </script>
